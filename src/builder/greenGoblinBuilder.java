ed300efcb47ed42563f9d21bd73ad82a59094513
zhannurr-png <253750@astanait.edu.kz>
2026-09-12 22:26:37 +0500

    create green goblin character builder

---
+++ src/builder/greenGoblinBuilder.java
@@ -0,0 +1,12 @@
+package builder;
+
+public class greenGoblinBuilder extends characterBuilder{
+    public greenGoblinBuilder(){
+        name = "Norman Osborn";
+        role = "Villain";
+        mainAbility = "Enhanced eflexes";
+        equipment = "Goblin glider";
+        mobility = "gliding";
+        specialAttack = "Pumpkin Bomb";
+    }
+}
