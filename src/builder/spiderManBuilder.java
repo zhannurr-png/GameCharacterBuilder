d63297a8e69ad7787e939624402852642b24644a
zhannurr-png <253750@astanait.edu.kz>
2026-09-12 22:16:20 +0500

    create spiderman character builder

---
+++ src/builder/spiderManBuilder.java
@@ -0,0 +1,12 @@
+package builder;
+
+public class spiderManBuilder extends characterBuilder{
+    public spiderManBuilder(){
+        name = "Peter Parker";
+        role = "Hero";
+        mainAbility = "Spider sense";
+        equipment = "Web shooters";
+        mobility = "wall crawling";
+        specialAttack = "web attack";
+    }
+}
